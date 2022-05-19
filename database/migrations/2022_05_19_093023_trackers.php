50681235275b0a41f6cf17f27cf86055d658f9a2
iamjnayem <iamj.nayem@gmail.com>
2022-05-19 15:32:29 +0600

    create tracker migration

---
+++ database/migrations/2022_05_19_093023_trackers.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trackers', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('name');
+            $table->json('type');
+            $table->foreignId('user_id');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trackers');
+    }
+};
